b44b0b2ab7dd54a7ae5c072c70b300d387b8d291
Alex <31661440+am2367@users.noreply.github.com>
2018-08-30 20:27:36 -0400

    trying out cloudant

--- api/models/getWeekly.js
+++ api/models/getWeekly.js
@@ -1,8 +1,14 @@
 const getWeekly = (req, callback) => {
     var MongoClient = require('mongodb').MongoClient;
+    var Cloudant = require('@cloudant/cloudant');
     let username = '';
     let password = '';
     let url = '';
+
+    var cloudUsername = process.env.cloudant_username;
+    var cloudPassword = process.env.cloudant_password;
+    var cloudant = Cloudant({account:cloudUsername, password:cloudPassword});
+    var mydb = cloudant.db.use('mydb')
     if (process.env.mLabUser){
         username = process.env.mLabUser;
         password = process.env.mLabPassword;
@@ -11,8 +17,21 @@ const getWeekly = (req, callback) => {
     else{
         url = "mongodb://localhost:27017/myapp";
     }
-    MongoClient.connect(url, function(err, db) {
+    //MongoClient.connect(url, function(err, db) {
         var query = {Date: { $gte: new Date(req.dateStart), $lte: new Date(req.dateEnd) } }
+        mydb.find(query).toArray(function myFunc(err, result) {
+            if (err) throw err;
+            
+            if(result.length == 7){
+                console.log(result)
+                callback(result);
+            }
+            else{
+                callback('Missing Days')
+            }    
+        
+            db.close();
+        });
         /*var query = {$and: 
                         [{Date: 
                             { $gte: new Date(req.dateStart), 
@@ -21,7 +40,7 @@ const getWeekly = (req, callback) => {
                             {Username: 'amarkenzon'}
                         ]};*/
 
-        if (err) throw err;
+        /*if (err) throw err;
         console.log("Database Connected!");
         
         var dbo = db.db("myapp");
@@ -39,7 +58,7 @@ const getWeekly = (req, callback) => {
         
             db.close();
         });
-    });
+    });*/
 }
 
 module.exports = getWeekly;
